8f2aafc149c778a130666bfa0d9129c1a19b91be
itoutsource.cz1731 <itoutsource.cz1731@trinasolar.com>
2026-01-09 17:00:44 +0800

    add runner

--- runner.go
+++ runner.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Runner struct {
+	engine gin.Engine
+}
+
+func NewRunner() *Runner {
+	engine := *gin.Default()
+
+	return &Runner{
+		engine: engine,
+	}
+}
